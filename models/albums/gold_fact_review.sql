6320a1b4065609a0d2b14ca2e3336833ba1f21fd
Andrew Crosher <andrewcrosher@outlook.com>
2024-10-05 12:28:30 +0100

    add a date dimension and gold fact table

---
+++ models/albums/gold_fact_review.sql
@@ -0,0 +1,12 @@
+select 
+  da.album as Album
+  , da.artist as Artist
+  , fr.my_rating as `My Rating`
+  , fr.global_rating as `Global Rating`
+  , cast(fr.my_rating / fr.global_rating as decimal(5,2)) as `My Relative Rating`
+  , fr.review_date as `Review Date`
+from
+  {{ ref('silver_fact_review') }} as fr
+inner join 
+  {{ ref("silver_dim_album")}} as da
+on fr.youtube_id = da.youtube_id
