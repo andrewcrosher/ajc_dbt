04ccf15205a927c137da055d55305a2691919588
Andrew Crosher <andrewcrosher@outlook.com>
2024-10-05 13:31:10 +0100

    add release year to gold

--- models/gold_fact_review.sql
+++ models/gold_fact_review.sql
@@ -1,6 +1,7 @@
 select 
   da.album as Album
   , da.artist as Artist
+  , cast(da.release_date as int) as `Release Year`
   , fr.my_rating as `My Rating`
   , fr.global_rating as `Global Rating`
   , cast(fr.my_rating / fr.global_rating as decimal(5,2)) as `My Relative Rating`
