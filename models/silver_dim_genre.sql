6f03bef3fdd786b3b674243baf386667bd92f30d
Andrew Crosher <andrewcrosher@outlook.com>
2025-01-18 16:22:12 +0000

    Add silver dimension for album genres and subgenres

---
+++ models/silver_dim_genre.sql
@@ -0,0 +1,30 @@
+with cte_all as (
+    select
+        h.album.youtubemusicid as youtube_id,
+        h.album.genres,
+        h.album.subgenres,
+        h.generatedat as reviewed_on
+    from
+        {{ ref('bronze_albums') }} lateral view explode(history) as h
+    order by
+        load_timestamp desc
+),
+
+cte_dedup as (
+    select distinct *
+    from
+        cte_all
+    order by
+        reviewed_on
+),
+
+cte_genres as (
+select
+  distinct genre
+  , subgenres
+from
+  cte_dedup lateral view explode(genres) as genre
+)
+
+SELECT *
+FROM cte_genres
