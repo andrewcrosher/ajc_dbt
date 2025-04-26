5b417529d2f41f9b3492d847c1ea33510c480a64
andrewcrosher <73142365+andrewcrosher@users.noreply.github.com>
2025-04-26 17:09:27 +0100

    Add decade to gold dbt model (#34)

--- dbt/models/gold_fact_review.sql
+++ dbt/models/gold_fact_review.sql
@@ -9,7 +9,8 @@ select
         (fr.my_rating / fr.global_rating) * 100 as decimal(5, 2)
     ) as `My Relative Rating`,
     fr.review_date as `Review Date`,
-    da.wikipedia_url as `Wikipedia Link`
+    da.wikipedia_url as `Wikipedia Link`,
+    concat(substring(da.release_date, 1, 3), '0s') as `Release Decade`
 from
     {{ ref('silver_fact_review') }} as fr
 inner join
